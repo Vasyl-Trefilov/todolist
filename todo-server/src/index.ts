b89e867339931f0aa3f46f684516197ce226c106
Vasyl-Trefilov <drenfor228@gmail.com>
2024-10-09 21:11:50 +0200

    last commit

--- todo-server/src/index.ts
+++ todo-server/src/index.ts
@@ -2,18 +2,24 @@ import express from "express";
 import mongoose from "mongoose";
 import Todo from "./models/todo";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config(); // Загружаем переменные окружения из .env
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 app.use(cors());
-
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://vasya-tref:Z4so6vL1MK6cjiXf@cluster0.qjag7.mongodb.net/todoBase?retryWrites=true&w=majority";
+const mongoUri = process.env.MONGODB_URI;
+
+if (!mongoUri) {
+  console.error("MONGODB_URI is not defined in .env file");
+  process.exit(1); // Завершаем процесс, если переменная не найдена
+}
 
 mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB: ", err));
 
